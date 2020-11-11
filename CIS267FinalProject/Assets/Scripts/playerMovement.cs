7c4a64027500a05127ba2c22629a61fb89691834
nimbusreal <60530055+nimbusreal@users.noreply.github.com>
2020-11-11 18:51:30 -0500

    Added a Grounding System for the Player Movement Script
    Added a function in the playerMovement script that detects the ground using raycasting. Also fixed player flipping so that just the sprite flips and not the whole object.

--- CIS267FinalProject/Assets/Scripts/playerMovement.cs
+++ CIS267FinalProject/Assets/Scripts/playerMovement.cs
@@ -12,10 +12,15 @@ public class playerMovement : MonoBehaviour
 
     private float inputHorizontal;
     private Rigidbody2D playerRigidBody;
+    private BoxCollider2D playerBoxCollider2D;
+    private SpriteRenderer playerSpriteRenderer;
+    [SerializeField] private LayerMask groundLayer; //the serialized field adds a dropdown to this scripts in which you can select the layer
 
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        playerBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -36,17 +41,21 @@ public class playerMovement : MonoBehaviour
     private void flipPlayer(float input)
     {
         if (input > 0)
-            transform.eulerAngles = new Vector2(0, 0);
+        {
+            playerSpriteRenderer.flipX = false;
+        }
 
         else if (input < 0)
-            transform.eulerAngles = new Vector2(0, 180);
+        {
+            playerSpriteRenderer.flipX = true;
+        }
     }
 
     private void jump()
     {
         //basic jump
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpVelocity);
         }
@@ -63,6 +72,22 @@ public class playerMovement : MonoBehaviour
         else if (playerRigidBody.velocity.y > 0 && !Input.GetButton("Jump"))
         {
             playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        float extraHeight = .1f;
+        //Draws a line slightly longer than our box collider to detect if we are grounded
+        RaycastHit2D raycastHit = Physics2D.Raycast(playerBoxCollider2D.bounds.center, Vector2.down, playerBoxCollider2D.bounds.extents.y + extraHeight, groundLayer);
+
+        if (raycastHit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
