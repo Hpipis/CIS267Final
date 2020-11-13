2c7e35ee93e9e3c4077552ad57044393a65b0039
nimbusreal <60530055+nimbusreal@users.noreply.github.com>
2020-11-13 03:13:00 -0500

    Added A* Pathfinding For Bat Enemy and Bat Flying Animation
    Imported Files for A*. This allows us to have a flying/grounded enemy to chase our player. Not sure if we will end up using this but it is a start to AI. Added the bat flying animation to make it look better also. Check it out in my test scene.

---
+++ CIS267FinalProject/Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aipath;
+
+    void Update()
+    {
+        if(aipath.desiredVelocity.x >= .01f)
+        {
+            transform.localScale = new Vector3(3f, 3f, 1f);
+        }
+        else if (aipath.desiredVelocity.x <= -.01f)
+        {
+            transform.localScale = new Vector3(-3f, 3f, 1f);
+        }
+    }
+}
