9a1654ca37627579f5a760e8b141f3e2a1d2b928
Hpipis <hpipis@my.monroeccc.edu>
2020-11-18 22:56:38 -0500

    Finished Level 1, Added Level Switch Script
    added script to switch from level 1 -> level 2  -> level 3 -> boss room (needs scene name of boss room to be added)

---
+++ CIS267FinalProject/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Scene currentLevel = SceneManager.GetActiveScene();
+            Debug.Log("Current Scene" + currentLevel.name);
+
+            if (currentLevel.name == "Level 1")
+            {
+                Debug.Log("You cleared Level 1, Enter Level 2");
+                SceneManager.LoadScene("Level 2");
+
+            }
+            else if (currentLevel.name == "Level 2")
+            {
+                Debug.Log("You cleared Level 2, Enter Level 3");
+                SceneManager.LoadScene("Level 3");
+
+            }
+            else if (currentLevel.name == "Level 3")
+            {
+                Debug.Log("You cleared Level 3, Enter Boss");
+                //enter boss scene name here 
+                //SceneManager.LoadScene("")
+            }
+        }
+    }
+}
