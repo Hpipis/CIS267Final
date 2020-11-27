4525672e0e358080309d23b0aa937e9ae2f9b866
bongardam <74200328+bongardam@users.noreply.github.com>
2020-11-27 10:47:53 -0500

    player sound
    jump sound has been removed from playerMovement and is in its own.

---
+++ CIS267FinalProject/Assets/Scripts/Sound/playerSoundFX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSoundFX : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+    private playerMovement pm;
+    //sound variables
+    public AudioClip playerJumpSound;
+    private AudioSource audioSource;
+
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    private void playJumpSOund()
+    {
+        if (Input.GetButtonDown("Jump") && pm.isGrounded())
+        {
+            audioSource.PlayOneShot(playerJumpSound);
+        }
+    }
+}
